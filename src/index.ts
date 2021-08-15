c97cc045da6a17e0b10eb191eda62bf0efc39cab
bucky2obs <our4000@gmail.com>
2021-08-16 01:08:20 +0900

    ..

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ export { default as Input } from "./input";
 export { default as Checkbox } from "./checkbox";
 export { default as Radio } from "./radio";
 export { default as Table } from "./table";
-export { default as UcThemeProvider, UcTheme } from "./uc-theme-provider";
+export {
+  default as UcThemeProvider,
+  UcTheme,
+  theme,
+} from "./uc-theme-provider";
 export { default as useModal } from "./useModal";
 export { default as useTab } from "./useTab";
 export { default as useSelect } from "./useSelect";
