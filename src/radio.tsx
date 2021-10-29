bed63c6da7c4d3def2c1ed1699ffa24a91bc43ba
anax015obs <our4000@gmail.com>
2021-10-29 15:02:55 +0900

    radio

--- src/radio.tsx
+++ src/radio.tsx
@@ -27,63 +27,55 @@ export default forwardRef<HTMLInputElement, Props>(function Radio(
     <div
       css={[
         css`
-          display: flex;
-          align-items: center;
+          display: inline-block;
         `,
         override,
       ]}
     >
-      <input
-        type="radio"
-        {...rest}
-        ref={ref}
-        css={css`
-          /* width: 14px;
-          height: 14px;
-          margin: 3px 3px 3px 4px; */
-          /* appearance: none; */
-          /* outline: none;
-          background-size: 100%; */
-          /* background-image: url(${UnCheckedSVG});
-          &:checked {
-            background-image: url(${CheckedSVG});
-          } */
-          background-color: #ffffff;
-          border: 1px solid #c8cace;
-          width: 14px;
-          height: 14px;
-          border-radius: 14px;
-          appearance: none;
-          outline: none;
-          margin: 3px 3px 3px 4px;
-          box-sizing: border-box;
-          position: relative;
-          &:checked {
-            border: 1px solid ${theme.palettes.primary._500};
-          }
-
-          &:checked:after {
-            width: 8px;
-            height: 8px;
-            border-radius: 8px;
-
-            top: 2px;
-            left: 2px;
-            position: absolute;
-            background-color: ${theme.palettes.primary._500};
-            content: "";
-            display: inline-block;
-            visibility: visible;
-          }
-        `}
-      />
       <label
         css={css`
+          cursor: pointer;
           font-size: 1.14rem;
-          margin-left: 8px;
+          display: flex;
+          align-items: center;
           color: ${theme.palettes.grey._1100};
         `}
       >
+        <input
+          type="radio"
+          {...rest}
+          ref={ref}
+          css={css`
+            cursor: pointer;
+            background-color: #ffffff;
+            border: 1px solid #c8cace;
+            width: 14px;
+            height: 14px;
+            border-radius: 14px;
+            appearance: none;
+            outline: none;
+            margin: 3px 11px 3px 4px;
+            box-sizing: border-box;
+            position: relative;
+            &:checked {
+              border: 1px solid ${theme.palettes.primary._500};
+            }
+
+            &:checked:after {
+              width: 8px;
+              height: 8px;
+              border-radius: 8px;
+
+              top: 2px;
+              left: 2px;
+              position: absolute;
+              background-color: ${theme.palettes.primary._500};
+              content: "";
+              display: inline-block;
+              visibility: visible;
+            }
+          `}
+        />
         {label}
       </label>
     </div>
